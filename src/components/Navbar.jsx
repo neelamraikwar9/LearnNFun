55a875cd2446479025878b0de1355385c83cb9a4
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-10-26 21:25:12 +0530

    Added styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,41 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <main>
-      {/* <h1>navbar</h1> */}
-      
       <nav className="navContainer">
-        <a href="">LNF</a>
-        <ul className="ulCon">
-          <li className="liStyl">
-            <NavLink to="" className="liText">Home</NavLink>
-          </li>
-          <li className="liStyl">
-            <NavLink to="" className="liText">About</NavLink>
-          </li>
-          <li className="liStyl">
-            <NavLink to="" className="liText">Member</NavLink>
-          </li>
-          
-          <li className="liStyl">
-            <NavLink to="" className="liText">Schedule</NavLink>
-          </li>
-        </ul>
+        <div className="navBar">
+          <div className="lnf">
+            <a href="/" className="lnf">
+              Learn 'N' Fun
+            </a>
+          </div>
+
+          <div>
+            <ul className="navItemCon">
+              <li className="liStyl">
+                <NavLink to="" className="liText">
+                  Home
+                </NavLink>
+              </li>
+              <li className="liStyl">
+                <NavLink to="" className="liText">
+                  About
+                </NavLink>
+              </li>
+              <li className="liStyl">
+                <NavLink to="" className="liText">
+                  Member
+                </NavLink>
+              </li>
+
+              <li className="liStyl">
+                <NavLink to="" className="liText">
+                  Schedule
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+
         <button className="joinUs">Join us!</button>
       </nav>
     </main>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,74 +1,159 @@
 import Navbar from "../components/Navbar";
 const LandingPage = () => {
-
-    return(
-        <main className="body">
-        <div className="background">
-           {/* <img src="/public/images/img.png" alt="backgroundimg"/> */}
+  return (
+    <main className="body">
+      <div className="background">
         <div className="navbar">
-        <Navbar/>
+          <Navbar />
         </div>
 
         <div className="lines">
-          <h1>Discover How We Enhance Your English Communication Skills</h1>
-          <p>Broaden your knowledge network to break free from life's constraints. </p>
-           <button className="">Learn More</button>
+          <h1 className="midh1" style={{}}>
+            Discover How We Enhance Your English Communication Skills
+          </h1>
+          <p className="midp1">
+            Broaden your knowledge network to break free from life's
+            constraints.{" "}
+          </p>
+          <button className="midBtn">Learn More</button>
         </div>
-        </div>
+      </div>
+      <br/>
+      
 
       
+
+
      
-   
-
-
-        
-        <br/>
-        <br/>
-
-        <div className="imgContainer">
-            <div className="imgSecCont">
-            {/* <img src="/public/images/img" alt="backgroundimg"/> */}
-            </div>
- 
-            <div className="shortInfoCon">
-            <h1>About Us</h1>
-            <p>lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+     <div className="container">    
+      <div className="vibrantCon">
+      <img src="https://res.cloudinary.com/dltfnhclh/image/upload/v1761486685/Untitled_design_ndthtg.png" alt="communityImg" className="imgCon"/>
+        <div className="vibText">
+            <h1 className="textHead">Our Vibrant English Community</h1>
+            <p className="miss">At Learn 'N' Fun, we're on a mission to revolutionize English communication through interactive and engaging classes.</p>
+            <br/>
+            <br/>
+    
             
-            </div>
-            </div>
+            <p><i>" Strength is Life, Weakness is Death. Expansion is Life, Contraction is Death. Love is Life, Hatred is Death. " </i></p>
+        </div>
+      </div>
 
-        
-        <div className="bigContainer">
-        <div className="missionStatement">
-        <h1 className="statement"><span style={{color: 'red'}}> Learn </span> <span style={{color: 'black'}}>'N'</span><span>Fun</span></h1>
+      <div className="iniContainer">
+        <h1 className="textHead initiator">The Initiator</h1>
+
+        <div className="iniCon">
+            <div className="iniImg">
+                <img src="https://res.cloudinary.com/dltfnhclh/image/upload/v1761490034/Untitled_design_1_vdzi09.png" alt="Shubham Pandey" className="Sh"/>
+            </div>
+            <div className="iniName">
+                <h2>Shubham Pandey</h2>
+            </div>
         </div>
-        <div className="missionStatement">
-        <h1 className="statement">ENGLISH FLUENCY <span style={{color: 'red'}}>THROUGH </span>CONVERSATION</h1>
-        </div>
-        <div style={{display: 'flex', justifyContent: 'space-around'}}>
-        <div style={{border: '1px solid red' }}>
-        <h2>Outcomes:</h2>
-        <p style={{margin: '0.5rem'}}>✔️Soft Skills</p>
-        <p style={{margin: '0.5rem'}}>✔️Communication Skills</p>
-        <p style={{margin: '0.5rem'}}>✔️Leadership Development</p>
-        <p style={{margin: '0.5rem'}}>✔️General Awareness</p>
-        </div>
+      </div>
+
+
+     <div className="iniContainer">
+        <h1 className="textHead initiator">Meet a Few of Our Community Members</h1>
  
-         <div style={{border: '1px solid red' }}>
-        <h2>Facilities:</h2>
-        <p style={{margin: '0.5rem'}}>✔️Access To All Grammer Materials</p>
-        <p style={{margin: '0.5rem'}}>✔️Access To More Than 100 Life Changing Novels</p>
-        <p style={{margin: '0.5rem'}}>✔️Virtual Learning Environment</p>
-        <p style={{margin: '0.5rem'}}>✔️Interaction With People From Various Aspect Of Life</p>
-        </div>
+        <div className="iniContainer  teContainer">
+        <div className="teCon">
+            <div className="">
+                <img src="https://res.cloudinary.com/dltfnhclh/image/upload/v1761490034/Untitled_design_1_vdzi09.png" alt="Shubham Pandey" className="Sh"/>
+            </div>
+            <div className="teAbout">
+                <h2>Shubham Pandey</h2>
+            </div>
         </div>
 
 
+        <div className="teCon">
+            <div className="">
+                <img src="https://res.cloudinary.com/dltfnhclh/image/upload/v1761490034/Untitled_design_1_vdzi09.png" alt="Shubham Pandey" className="Sh"/>
+            </div>
+            <div className="teAbout">
+                <h2>Shubham Pandey</h2>
+            </div>
         </div>
+        
+
+
+        <div className="teCon">
+            <div className="">
+                <img src="https://res.cloudinary.com/dltfnhclh/image/upload/v1761490034/Untitled_design_1_vdzi09.png" alt="Shubham Pandey" className="Sh"/>
+            </div>
+            <div className="teAbout">
+                <h2>Shubham Pandey</h2>
+            </div>
+        </div>
+
+
+        <div className="teCon">
+            <div className="">
+                <img src="https://res.cloudinary.com/dltfnhclh/image/upload/v1761490034/Untitled_design_1_vdzi09.png" alt="Shubham Pandey" className="Sh"/>
+            </div>
+            <div className="teAbout">
+                <h2>Shubham Pandey</h2>
+            </div>
+        </div>
+
+        <div className="teCon">
+            <div className="">
+                <img src="https://res.cloudinary.com/dltfnhclh/image/upload/v1761490034/Untitled_design_1_vdzi09.png" alt="Shubham Pandey" className="Sh"/>
+            </div>
+            <div className="teAbout">
+                <h2>Shubham Pandey</h2>
+            </div>
+        </div>
+        </div>
+
 
         
-        </main>
-    )
-}
+      
+      </div>
+
+      </div>
+
+      {/* <div className="bigContainer">
+        <div className="missionStatement">
+          <h1 className="statement">
+            <span style={{ color: "red" }}> Learn </span>{" "}
+            <span style={{ color: "black" }}>'N'</span>
+            <span>Fun</span>
+          </h1>
+        </div>
+        <div className="missionStatement">
+          <h1 className="statement">
+            ENGLISH FLUENCY <span style={{ color: "red" }}>THROUGH </span>
+            CONVERSATION
+          </h1>
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <div style={{ border: "1px solid red" }}>
+            <h2>Outcomes:</h2>
+            <p style={{ margin: "0.5rem" }}>✔️Soft Skills</p>
+            <p style={{ margin: "0.5rem" }}>✔️Communication Skills</p>
+            <p style={{ margin: "0.5rem" }}>✔️Leadership Development</p>
+            <p style={{ margin: "0.5rem" }}>✔️General Awareness</p>
+          </div>
+
+          <div style={{ border: "1px solid red" }}>
+            <h2>Facilities:</h2>
+            <p style={{ margin: "0.5rem" }}>
+              ✔️Access To All Grammer Materials
+            </p>
+            <p style={{ margin: "0.5rem" }}>
+              ✔️Access To More Than 100 Life Changing Novels
+            </p>
+            <p style={{ margin: "0.5rem" }}>✔️Virtual Learning Environment</p>
+            <p style={{ margin: "0.5rem" }}>
+              ✔️Interaction With People From Various Aspect Of Life
+            </p>
+          </div>
+        </div>
+      </div> */}
+    </main>
+  );
+};
 
 export default LandingPage;
