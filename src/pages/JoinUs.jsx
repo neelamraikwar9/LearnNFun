4832bb9b92d415cebd3b47feedacbe3a7258e2ab
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-11-07 11:47:11 +0530

    fixed layout with styles

--- src/pages/JoinUs.jsx
+++ src/pages/JoinUs.jsx
@@ -67,44 +67,44 @@ const JoinUs = () => {
       <form onSubmit={handleSubmit} className="formCon">
         <div className='field'>
         <label htmlFor="nam">Name</label>
-        <input type="text" id="nam" name="name" value={form.name} onChange={handleOnChange} className="inp"/>
+        <input type="text" id="nam" name="name" value={form.name} onChange={handleOnChange} className="inp" autoComplete="name"/>
         </div>
 
         <div className='field'>
         <label htmlFor="em">Email</label>
-        <input type="text" id="em" name="email" value={form.email} onChange={handleOnChange} className="inp"/>
+        <input type="text" id="em" name="email" value={form.email} onChange={handleOnChange} className="inp" autoComplete="email"/>
         </div>
 
 
          <div className='field'>
         <label htmlFor="cont">Contact Number</label>
-        <input type="number" id="cont" name="contactNumber" value={form.contactNumber} onChange={handleOnChange} className="inp"/>
+        <input type="number" id="cont" name="contactNumber" value={form.contactNumber} onChange={handleOnChange} className="inp" autoComplete="contactNumber"/>
         </div>
 
         <div className='field'>
         <label htmlFor="cit">City</label>
-        <input type="text" id="cit" name="city" value={form.city} onChange={handleOnChange} className="inp"/>
+        <input type="text" id="cit" name="city" value={form.city} onChange={handleOnChange} className="inp" autoComplete="city"/>
         </div>
 
          <div className='field'>
         <label htmlFor="occ">Occupation </label>
-        <input type="text" id="occ" name="occupation" value={form.occupation} onChange={handleOnChange} className="inp"/>
+        <input type="text" id="occ" name="occupation" value={form.occupation} onChange={handleOnChange} className="inp" autoComplete="occupation"/>
         </div>
 
 
          <div className='field'>
         <label htmlFor="qul">Qualification </label>
-        <input type="text" id="qul" name="qualification" value={form.qualification} onChange={handleOnChange} className="inp"/>
+        <input type="text" id="qul" name="qualification" value={form.qualification} onChange={handleOnChange} className="inp" autoComplete="qualification"/>
         </div>
 
          <div className='field'>
         <label htmlFor="pur">Purpose</label>
-        <input type="text" id="pur" name="purpose" value={form.purpose} onChange={handleOnChange} className="inp"/>
+        <input type="text" id="pur" name="purpose" value={form.purpose} onChange={handleOnChange} className="inp" autoComplete="purpose"/>
         </div>
 
         <div className='field'>
           <label htmlFor="dur">Duration</label>
-          <select id="dur" name="duration" value={form.duration} onChange={handleOnChange} className="sel">
+          <select id="dur" name="duration" value={form.duration} onChange={handleOnChange} className="sel" autoComplete="off">
             <option value="One month">One month</option>
             <option value="Two month">Two month</option>
             <option value="Three month">Three month</option>
