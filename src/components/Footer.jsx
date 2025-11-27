65fb3bf24b6872fda1ca3927820d70d9c4d3f71b
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-11-27 23:26:30 +0530

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footerCon">
+
+    <div className="footerBox">
+    <div className="footCon">
+      <i class="bi bi-telephone-fill" style={{padding: '0', marginTop: '2rem', color: "greenyellow"}}></i>
+       <a href="tel:+919015256338" style={{ marginTop: "2rem" }}>+91  90152 56338</a>
+    </div>
+
+    <div className="footCon">
+      <i class="bi bi-envelope-at-fill" style={{padding: '0', marginTop: '2rem', marginLeft: '2rem', color: "greenyellow"}}></i>
+      <a href="mailto:learnnfun3@gmail.com" style={{marginTop:'2rem'}}>learnnfun3@gmail.com</a>
+    </div>
+    </div>
+    <div className="rightsCon">
+    <p>&copy; 2025 | Learn N Fun, All Rights Reserved.</p>
+    </div>
+    </footer>
   )
 }
 
